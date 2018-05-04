f20175611454e3209e567b881667114f74ea64c4
cl4rity <clara.engman@gmail.com>
2018-05-04 13:46:30 +0200

    Uppdaterad beskrivning

--- django/accounts/models.py
+++ django/accounts/models.py
@@ -5,9 +5,9 @@ from django.db import models
 class CustomUser(AbstractUser):
     # First/last name is not a global-friendly pattern
     name = models.CharField('Namn', max_length=255)
-    phone_no = models.IntegerField('Telefonnummer', null = True) # Remove null = True when a superuser is created
-    coname = models.CharField('Företagsnamn', max_length=80, default = '', null = True) # Remove null = True when a superuser is created
-    reg_no = models.CharField('Organisationsnummer', max_length=13, default = '', null = True) # Remove null = True when a superuser is created
+    phone_no = models.IntegerField('Telefonnummer', null = True) # Remove null = True when a superuser has been created
+    coname = models.CharField('Företagsnamn', max_length=80, default = '', null = True) # Remove null = True when a superuser has been created
+    reg_no = models.CharField('Organisationsnummer', max_length=13, default = '', null = True) # Remove null = True when a superuser has been created
 
     def __str__(self):
         return self.email
