a8e9a57f25c1b3cdcf287fa61424278453e31783
cl4rity <clara.engman@gmail.com>
2018-05-04 13:07:03 +0200

    Gjorde default på orgnr till en tom string

--- django/accounts/models.py
+++ django/accounts/models.py
@@ -7,7 +7,7 @@ class CustomUser(AbstractUser):
     name = models.CharField('Namn', max_length=255)
     phone_no = models.IntegerField('Telefonnummer', null = True) # Ta bort null = True när databasen har en superuser
     coname = models.CharField('Företagsnamn', max_length=80, default = '', null = True) # Ta bort null = True när databasen har en superuser
-    reg_no = models.CharField('Organisationsnummer', max_length=13, null = True, default = 'xxxxxxxx-xxxx') # Ta bort null = True när databasen har en superuser
+    reg_no = models.CharField('Organisationsnummer', max_length=13, null = True, default = '') # Ta bort null = True när databasen har en superuser
 
     def __str__(self):
         return self.email
