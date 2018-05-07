49e3fd196dedec78a09ae77ac61c28ad08ce6930
linneaskardin <linneaskardin@gmail.com>
2018-05-07 13:51:22 +0200

    ändringar i login

--- django/menuapp/components/Mainmenuauth.jsx
+++ django/menuapp/components/Mainmenuauth.jsx
@@ -11,11 +11,11 @@ export default class Mainmenu extends React.Component {
           <Nav class="navbar navbar-dark bg-primary">
             <NavItem href="/">Hem</NavItem>
             {/* This link only wokrs if you are on the homepage or on toolgate_maps */}
-            <NavItem href="toolgate_maps/punkter_pa_karta">Toolgate Maps</NavItem>       
+            <NavItem href="/toolgate_maps/punkter_pa_karta">Toolgate Maps</NavItem>       
             <NavItem>Mitt konto</NavItem>
           </Nav>
           <Nav pullRight>
-            <NavItem href="accounts/logout">Logga Ut</NavItem>
+            <NavItem href="/accounts/logout">Logga Ut</NavItem>
           </Nav>
           </Navbar>
     )
