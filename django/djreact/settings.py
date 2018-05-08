5ec2e311ff275c1a9c87912b9ef9db2e1bdc8a03
Gillof <34275614+Gillof@users.noreply.github.com>
2018-05-08 14:02:51 +0200

    fixat kommatecken

--- django/djreact/settings.py
+++ django/djreact/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap4'
+    'bootstrap4',
     'webpack_loader',
 
     'jquery',
