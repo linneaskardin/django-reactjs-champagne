ee8d878d05234a9abdf50a16ea42e61d99bb7959
Gillof <34275614+Gillof@users.noreply.github.com>
2018-05-08 14:25:20 +0200

    funkar fortfarande

--- django/menuapp/components/Mainmenuauth.jsx
+++ django/menuapp/components/Mainmenuauth.jsx
@@ -1,23 +1,29 @@
-// This is a main menu component. 
+// This is a main menu component.
 import React from "react"
 import { Link } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 
-export default class Mainmenu extends React.Component {
-  render() {
-    return (
-          <Navbar fluid collapseOnSelect>
-          <Nav>
-            <NavItem href="/">Hem</NavItem>
-            {/* This link only wokrs if you are on the homepage or on toolgate_maps */}
-            <NavItem href="toolgate_maps/punkter_pa_karta">Toolgate Maps</NavItem>       
-            <NavItem>Mitt konto</NavItem>
-          </Nav>
-          <Nav pullRight>
-            <NavItem href="accounts/logout">Logga Ut</NavItem>
-          </Nav>
-          </Navbar>
-    )
-  }
+export default class Mainmenuauth extends React.Component {
+ render() {
+   return (
+     <Nav className="navbar navbar-inverse bg-light">
+   <div className="container-fluid">
+   <div className="navbar-header">
+     <a className="navbar-brand">Toolgate Maps</a>
+   </div>
+   <ul className="nav navbar-nav">
+     <li><a href="/"> Hem </a></li>
+     <li><a href= "/toolgate_maps/punkter_pa_karta"> Toolgate Maps </a></li>
+     {/* <li><a href="/accounts/signup"><span className="glyphicon glyphicon-user"></span> Skapa Konto </a></li> */}
+   </ul>
+   <ul className="nav navbar-nav navbar-right">  
+     <li><a href="/accounts/logout"><span className="glyphicon glyphicon-log-in"></span> Logga Ut </a></li>
+   </ul>
+ </div>
+</Nav>
+   )
+ }
 }
+
+
