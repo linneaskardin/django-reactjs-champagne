ef639a889c850256e494bb9c7cf505972d8c7377
Gillof <34275614+Gillof@users.noreply.github.com>
2018-05-09 16:14:31 +0200

    pillar med att byta ikon vid klick.

--- django/djreact/settings.py
+++ django/djreact/settings.py
@@ -103,7 +103,7 @@ LEAFLET_CONFIG ={ #Ingrid: Detta fixar med kartan.
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'postgres5i',
+        'NAME': 'postgres6i',
         'USER': 'postgres',
         'PASSWORD': '123456',
         'HOST': '127.0.0.1',
