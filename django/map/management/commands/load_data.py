6c6cb2437d7e88e69a5d20788b23e8c379a859cf
cl4rity <clara.engman@gmail.com>
2018-05-10 16:46:50 +0200

    Köpeskilling laddas i load_data

--- django/map/management/commands/load_data.py
+++ django/map/management/commands/load_data.py
@@ -3,73 +3,115 @@ from django.core.management.base import BaseCommand, CommandError
 
 class Command(BaseCommand):
 
-    help = 'Populates models'
+    help = 'Populates model fields'
 
     def handle(self, *args, **options):
-        # Put everything that was originally in the python script. Can't include functions.
-        # Everything needs to be indented //CE
         import pyproj
         from map.models import Property, PropertyOwner
         from django.contrib.gis.geos import Point
         import csv
         # Import data for coordinates
-        csvkoord= open('data/MEDIAN_09M.txt', 'r', encoding='mac_roman', newline='') # Need encoding mac_roman and newline=''. Don't know why //CE
-        readerkoord = csv.reader(csvkoord, delimiter=';')
-        dictCoord={} # create dictionary to connect data
-        wgs84=pyproj.Proj("+init=EPSG:4326") # LatLon with WGS84 datum used by GPS units and Google Earth
-        SWEREF99=pyproj.Proj("+init=EPSG:3006") # SWEREF 99 TM system
-        for row in readerkoord:
+        with open('data/MEDIAN_09M.txt', 'r', encoding='mac_roman', newline='') as csvmed: # Need encoding mac_roman and newline=''. Don't know why //CE
+            readerkoord = csv.reader(csvmed, delimiter=';')
+            dictCoord={} # create dictionary to connect data
+            wgs84=pyproj.Proj("+init=EPSG:4326") # LatLon with WGS84 datum used by GPS units and Google Earth
+            SWEREF99=pyproj.Proj("+init=EPSG:3006") # SWEREF 99 TM system
+            for row in readerkoord:
             # Define projections using EPSG codes
-            wgs_e, wgs_n = pyproj.transform(SWEREF99,wgs84, row[6], row[5])
+                wgs_e, wgs_n = pyproj.transform(SWEREF99,wgs84, row[6], row[5])
             # Create Point
-            pnt = Point(wgs_e, wgs_n)
-            dictCoord[row[3]] = {'med_coord':pnt, 'coord_e':row[6], 'coord_n':row[5]} # Populate dictionary
-        csvkoord.close() # VERY important to close!
+                pnt = Point(wgs_e, wgs_n)
+                dictCoord[row[3]] = {'med_coord':pnt, 'coord_e':row[6], 'coord_n':row[5]} # Populate dictionary
         # Import data for Property Owners
-        csvlag = open('data/LAGFP_35S.txt','r', encoding='mac_roman',newline='')
-        readerl = csv.reader(csvlag, delimiter=';')
-        dictOwner={} # create dictionary to connect data
-        for row in readerl:
-            dictOwner[row[20]] = {'reg_nr':row[21],
-            'firstname':row[23], 'surname':row[25],'irfast':row[17],
-            'jurform':row[26], 'coname':row[27]}
-        csvlag.close()
-        csvar = open('data/AREAL_08A.txt','r', encoding='mac_roman',newline='')
-        readera = csv.reader(csvar, delimiter=';')
-        dictArea={}
-        for row in readera:
-            dictArea[row[3]] = {'area':row[7]}
-        csvar.close()
-        csvreg = open('data/REGENH_01A.txt','r', encoding='mac_roman',newline='')
-        readerr = csv.reader(csvreg, delimiter=';')
-        dictPropNo={}
-        for row in readerr:
-            dictPropNo[row[3]] = {'municipality':row[6],'district':row[7],'block':row[8],'sign':row[9],'unity':row[10]}
-        csvreg.close()
+        with open('data/LAGFP_35S.txt','r', encoding='mac_roman',newline='') as csvlag:
+            readerl = csv.reader(csvlag, delimiter=';')
+            dictOwner={} # create dictionary to connect data
+            for row in readerl:
+                dictOwner[row[20]] = {'reg_nr':row[21],
+                'firstname':row[23], 'surname':row[25],'irfast_uuid':row[17],
+                'jurform':row[26], 'coname':row[27]}
+
+        with open('data/AREAL_08A.txt','r', encoding='mac_roman',newline='') as csvar:
+            readera = csv.reader(csvar, delimiter=';')
+            dictArea={}
+            for row in readera:
+                dictArea[row[3]] = {'area':row[7]}
+
+        with open('data/REGENH_01A.txt','r', encoding='mac_roman',newline='') as csvreg:
+            readerr = csv.reader(csvreg, delimiter=';')
+            dictPropNo={}
+            for row in readerr:
+                dictPropNo[row[3]] = {'municipality':row[6],'district':row[7],'block':row[8],'sign':row[9],'unity':row[10]}
+
+        # Lägg in if-sats på alla som skulle kunna vara null så inte de kommer med i dictionaryt
+        with open('data/FASTAGF_35O.txt','r', encoding='mac_roman',newline='') as csvfas:
+            readerf = csv.reader(csvfas, delimiter=';')
+            dictFas={}
+            for row in readerf:
+                if row[5] is not None: # It might be None
+                    dictFas[row[5]] = {'fnr':row[7]}
+
+        with open('data/KOPESK_35P.txt','r', encoding='mac_roman',newline='') as csvkop:
+            readerk = csv.reader(csvkop, delimiter=';')
+            dictKop={}
+            for row in readerk:
+                dictKop[row[3]] = {'currency_fa':row[9],'price_fa':row[10],'currency_lo':row[15],'price_lo':row[16]}
+        # Merge dictKop and dictFas
+        d = {}
+        for key,value in dictKop.items():
+            if key in dictFas:
+                value = {**dictKop[key], **dictFas[key]}
+                d[key] = value
+        # Create dictPrice with fnr as a key
+        dictPrice = {}
+        for key,value in d.items():
+            dictPrice[d.get(key,{'fnr':'NA'})['fnr']] = {'currency_fa':d.get(key,{'currency_fa':'NA'})['currency_fa'],
+            'price_fa':d.get(key,{'price_fa':'NA'})['price_fa'],'currency_lo':d.get(key,{'currency_lo':'NA'})['currency_lo'],
+            'price_lo':d.get(key,{'price_lo':'NA'})['price_lo']}
+        # Merge all dictionaries
         z = {}
         for key,value in dictOwner.items():# iterator over e
             if key in dictCoord: # some PropertyOwners don't have coordinates
-                if key in dictArea:
-                    value = {**dictCoord[key], **dictOwner[key], **dictPropNo[key], **dictArea[key]} # pull values and merge them
+                if key in dictArea and key in dictPrice:
+                    value = {**dictCoord[key], **dictOwner[key], **dictPropNo[key], **dictArea[key], **dictPrice[key]} # pull values and merge them
+                elif key in dictArea and key not in dictPrice:
+                    value = {**dictCoord[key], **dictOwner[key], **dictPropNo[key], **dictArea[key]}
+                elif key not in dictArea and key in dictPrice:
+                    value = {**dictCoord[key], **dictOwner[key], **dictPropNo[key], **dictPrice[key]}
                 else:
                     value = {**dictCoord[key], **dictOwner[key], **dictPropNo[key]} # pull values and merge them
                 z[key] = value # add the new values to z
-        # populate database tables
+        # Populate model fields
         for key,value in z.items():
             q = PropertyOwner(reg_no=z.get(key,{'reg_nr':'NA'})['reg_nr'],
             firstname=z.get(key,{'firstname':'NA'})['firstname'],surname=z.get(key,{'surname':'NA'})['surname'],
             coname=z.get(key,{'coname':'NA'})['coname'],jurform=z.get(key,{'jurform':'NA'})['jurform'])
             q.save()
-            if key in dictArea:
+            if key in dictArea and key in dictPrice:
                 y = Property(med_coord=z.get(key,{'med_coord':'NA'})['med_coord'], coord_e=z.get(key,{'coord_e':'NA'})['coord_e'],
                 coord_n=z.get(key,{'coord_n':'NA'})['coord_n'], area=z.get(key,{'area':'NA'})['area'], municipality=z.get(key,{'municipality':'NA'})['municipality'],
                 district=z.get(key,{'district':'NA'})['district'],block=z.get(key,{'block':'NA'})['block'],sign=z.get(key,{'sign':'NA'})['sign'],
-                unity=z.get(key,{'unity':'NA'})['unity'])
+                unity=z.get(key,{'unity':'NA'})['unity'],price_fa=z.get(key,{'price_fa':'NA'})['price_fa'],currency_fa=z.get(key,{'currency_fa':'NA'})['currency_fa'],
+                price_lo=z.get(key,{'price_lo':'NA'})['price_lo'],currency_lo=z.get(key,{'currency_lo':'NA'})['currency_lo'])
+            # Price becomes 'okänd' when info doesn't exist
+            elif key in dictArea and key not in dictPrice:
+                y = Property(med_coord=z.get(key,{'med_coord':'NA'})['med_coord'], coord_e=z.get(key,{'coord_e':'NA'})['coord_e'],
+                coord_n=z.get(key,{'coord_n':'NA'})['coord_n'], area=z.get(key,{'area':'NA'})['area'], municipality=z.get(key,{'municipality':'NA'})['municipality'],
+                district=z.get(key,{'district':'NA'})['district'],block=z.get(key,{'block':'NA'})['block'],sign=z.get(key,{'sign':'NA'})['sign'],
+                unity=z.get(key,{'unity':'NA'})['unity'],price_fa='okänd',price_lo='okänd')
+            elif key not in dictArea and key in dictPrice:
+                y = Property(med_coord=z.get(key,{'med_coord':'NA'})['med_coord'], coord_e=z.get(key,{'coord_e':'NA'})['coord_e'],
+                coord_n=z.get(key,{'coord_n':'NA'})['coord_n'], municipality=z.get(key,{'municipality':'NA'})['municipality'],
+                district=z.get(key,{'district':'NA'})['district'],block=z.get(key,{'block':'NA'})['block'],sign=z.get(key,{'sign':'NA'})['sign'],
+                unity=z.get(key,{'unity':'NA'})['unity'],price_fa=z.get(key,{'price_fa':'NA'})['price_fa'],currency_fa=z.get(key,{'currency_fa':'NA'})['currency_fa'],
+                price_lo=z.get(key,{'price_lo':'NA'})['price_lo'],currency_lo=z.get(key,{'currency_lo':'NA'})['currency_lo'])
             else:
                 y = Property(med_coord=z.get(key,{'med_coord':'NA'})['med_coord'], coord_e=z.get(key,{'coord_e':'NA'})['coord_e'],
-                coord_n=z.get(key,{'coord_n':'NA'})['coord_n'],area=0,municipality=z.get(key,{'municipality':'NA'})['municipality'],
+                coord_n=z.get(key,{'coord_n':'NA'})['coord_n'], municipality=z.get(key,{'municipality':'NA'})['municipality'],
                 district=z.get(key,{'district':'NA'})['district'],block=z.get(key,{'block':'NA'})['block'],sign=z.get(key,{'sign':'NA'})['sign'],
-                unity=z.get(key,{'unity':'NA'})['unity']) # Area becomes 0 when info of the area doesn't exist
+                unity=z.get(key,{'unity':'NA'})['unity'],price_fa='okänd',price_lo='okänd')
             y.save()
             y.owners.add(q)
+
+
         self.stdout.write("Successfully populated models", ending='') # This is the way to print in the console //CE
