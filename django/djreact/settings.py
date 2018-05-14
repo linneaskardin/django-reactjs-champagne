f64be939412abf492a36bba052478f68dc281f8f
linneaskardin <linneaskardin@gmail.com>
2018-05-14 10:24:23 +0200

    efter merge med master och databas postgres7l

--- django/djreact/settings.py
+++ django/djreact/settings.py
@@ -103,7 +103,7 @@ LEAFLET_CONFIG ={ #Ingrid: Detta fixar med kartan.
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'postgres4l',
+        'NAME': 'postgres7l',
         'USER': 'postgres',
         'PASSWORD': '123456',
         'HOST': '127.0.0.1',
