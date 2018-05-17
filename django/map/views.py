47cd63079b6b8318392d00b56c361740708bcbb6
Gillof <34275614+Gillof@users.noreply.github.com>
2018-05-17 16:56:12 +0200

    Sparar å gåt hem

--- django/map/views.py
+++ django/map/views.py
@@ -8,15 +8,14 @@ from .models import Punkt, Waypoint, Property, PropertyBoarder, PropertyOwner, L
 import json
 from django.http import HttpResponse, HttpResponseNotFound
 from django.core.serializers import serialize
+from django.contrib.gis.measure import D
+from django.contrib.gis.geos import GEOSGeometry, Point
 
 def index(request):
-    print('views.py 1')
     return render(request, 'map/index.html')
 
 def punkt_datasets(request):
-    print("views.py 1")
     punkter = serialize('geojson', Punkt.objects.all())
-    print("views.py 2")
     print (punkter)
     return HttpResponse(punkter, content_type='json')
 
@@ -26,11 +25,29 @@ def waypoint_datasets(request):
 
 def property_datasets(request):
     #punkter = serialize('geojson', Property.objects.all())
+    
+    print("i punkters view.")
 
-    punkter = serialize('geojson', Property.objects.filter(pk__lte=30)) #gte = greater/equal than, lte = less/equal than
-    #Property.objects.filter(coord_n__lte=6740000))
-
-    return HttpResponse(punkter, content_type='json')
+    
+    if (request.method == 'POST'):
+        print("i if")
+        print("it is a POST-anrop i property-viewen")
+        centerLat = float(request.POST.get('centerLat'))
+        centerLng = float(request.POST.get('centerLng'))
+        print(centerLat)
+        print(centerLng)
+        
+        point = Point(centerLng, centerLat)
+        pnt = GEOSGeometry(point, srid=4326)
+        print(pnt)
+        
+        thePoints = serialize('geojson', Property.objects.filter(med_coord__distance_lte=(pnt, D(m=200))))
+        return HttpResponse(thePoints, content_type='json')
+    
+#    else:
+#        print("i else")
+#        punkter = serialize('geojson', Property.objects.filter(pk__lte=30)) #gte = greater/equal than, lte = less/equal than
+#        return HttpResponse(punkter, content_type='json')
 
 def propertyOwner_datasets(request):
     #punkter = serialize('geojson', Property.objects.all())
