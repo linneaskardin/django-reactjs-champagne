623cc15e8767c53ef942591e22bab9fad4a89916
Gillof <34275614+Gillof@users.noreply.github.com>
2018-05-21 09:36:46 +0200

    lleaseholders o rätt ägare funkar!

--- django/map/views.py
+++ django/map/views.py
@@ -11,6 +11,8 @@ from django.core.serializers import serialize
 from django.contrib.gis.measure import D
 from django.contrib.gis.geos import GEOSGeometry, Point
 
+from pprint import pprint
+
 def index(request):
     return render(request, 'map/index.html')
 
@@ -29,42 +31,51 @@ def property_datasets(request):
     if (request.method == 'POST'):
         centerLat = float(request.POST.get('centerLat'))
         centerLng = float(request.POST.get('centerLng'))
-        
         point = Point(centerLng, centerLat)
         pnt = GEOSGeometry(point, srid=4326)
         print(pnt)
         
-        print("jomjom")
-        print(Property.objects.filter(med_coord__distance_lte=(pnt, D(m=200))))
-        print("hejsvej")
-        
-        thePoints = serialize('geojson', Property.objects.filter(med_coord__distance_lte=(pnt, D(m=200)))) #the raidious given should be the same as in propertyOwner_datasets.
-        return HttpResponse(thePoints, content_type='json')
+        propertyGEOJson = serialize('geojson', Property.objects.filter(med_coord__distance_lte=(pnt, D(m=280)))) #the raidious given should be the same as in propertyOwner_datasets.
+        return HttpResponse(propertyGEOJson, content_type='json')
 
 def propertyOwner_datasets(request):
     print("i owners view")
    # if (request.method == 'POST'):
     centerLat = float(request.POST.get('centerLat'))
     centerLng = float(request.POST.get('centerLng'))
-    
     point = Point(centerLng, centerLat)
     pnt = GEOSGeometry(point, srid=4326)
     print(pnt)
 
-    thePropertiesInRange =Property.objects.filter(med_coord__distance_lte=(pnt, D(m=200))) #the raidious given should be the same as in property_datasets.
-    propertiesID = []
+    thePropertiesInRange =Property.objects.filter(med_coord__distance_lte=(pnt, D(m=280))) #the raidious given should be the same as in property_datasets.
+    ownersID = []
     for aProperty in thePropertiesInRange:
-        ownersPk=aProperty.pk
-        propertiesID.append(ownersPk)
-        #owners.append(PropertyOwner.objects.filter(pk__exact=ownersPk)) #16112 
-    
-    ownersGEOJson = serialize('geojson', PropertyOwner.objects.filter(pk__in=propertiesID)) 
+        theOwners = aProperty.owners.all()
+        for owner in theOwners:
+            ownersID.append(owner.pk)
+         
+    ownersGEOJson = serialize('geojson', PropertyOwner.objects.filter(pk__in=ownersID)) 
     return HttpResponse(ownersGEOJson, content_type='json')
 
 def leaseHolder_datasets(request):
-    #punkter = serialize('geojson', Property.objects.all())
-    punkter = serialize('geojson',  LeaseHolder.objects.filter(pk__lte=300)) #gte = greater/equal than, lte = less/equal than
-    return HttpResponse(punkter, content_type='json')
+    
+    centerLat = float(request.POST.get('centerLat'))
+    centerLng = float(request.POST.get('centerLng'))
+    point = Point(centerLng, centerLat)
+    pnt = GEOSGeometry(point, srid=4326)
+    print(pnt)
+
+    thePropertiesInRange =Property.objects.filter(med_coord__distance_lte=(pnt, D(m=280))) #the raidious given should be the same as in property_datasets.
+    leaseHoldersID = []
+    
+    for aProperty in thePropertiesInRange:
+        
+        theLeasers = aProperty.leaseholders.all()
+        for leaser in theLeasers:
+            leaseHoldersID.append(leaser.pk)
+
+    leasersGEOJson = serialize('geojson', LeaseHolder.objects.filter(pk__in=leaseHoldersID))
+    return HttpResponse(leasersGEOJson, content_type='json')
 
 def propertyBoarder_datasets(request):
     #punkter = serialize('geojson', PropertyBoarder.objects.all())
@@ -72,13 +83,9 @@ def propertyBoarder_datasets(request):
     return HttpResponse(punkter, content_type='json')
 
 
-def makePointFromCoord():
-    return null
-
 
 
 ###########SPARAD KOD##########
-
 
 #    punkter = serialize('geojson', PropertyOwner.objects.filter(pk__lte=100)) #gte = greater/equal than, lte = less/equal than
 #    #punkter = serialize('geojson', Property.objects.all())
